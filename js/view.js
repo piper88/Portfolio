29340a130401190e7c8ce23d567b946c94bb4b0c
Sarah DeBey <sarah.debey@gmail.com>
2016-06-12 12:03:08 -0700

    add populate filter function

--- js/view.js
+++ js/view.js
@@ -8,13 +8,36 @@ projectView.handleNav = function() {
   $('.main-nav .tab:first').click();
 };
 
+projectView.populateFilter = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).find('h2').text();
+      console.log(val);
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#project-filter').append(optionTag);
+    }
+  });
+};
+
+projectView.handleFilter = function() {
+  $('#project-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      //find the article with the h2 value equal to $(this).val, and fade that article in
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+      var targetProject = $('article').find('h2')
+    }
+  });
+};
+
 projectView.handleTeasers = function() {
   $('.projects-body *:nth-of-type(n+2)').hide();
   $('#projects').on('click', 'a.read-on', function(e) {
     e.preventDefault();
     $(this).parent().find('*').show();
     $(this).hide();
-    $(this).parent().$('.show-less').show();
+    // $(this).parent().$('.show-less').show();
+    // $('.show-less').show();
   });
   $('#projects').on('click', 'a.show-less', function(e) {
     e.preventDefault();
@@ -26,3 +49,4 @@ projectView.handleTeasers = function() {
 
 projectView.handleNav();
 projectView.handleTeasers();
+projectView.populateFilter();
